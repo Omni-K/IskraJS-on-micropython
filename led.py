0484a96216a901466613910538167a4c3a11a23e
Путько Николай <reg@omni-k.ru>
2022-04-03 12:19:50 +0300

    docstrings

--- led.py
+++ led.py
@@ -3,31 +3,54 @@ from machine import Pin
 
 
 class LED(Pin):
-    is_on = False
-    pin = ''
+    """
+    Класс для управления светодиодом
+    """
+    __slots__ = ['is_on', 'pin']
 
-    def __init__(self, pid:str):
+    def __init__(self, pid: str):
         super().__init__(pid, Pin.OUT)
+        self.is_on = False
         self.pin = pid
         self.value(0)
 
     def value(self, v):
+        """
+        Включает / выключает светодиод
+        1 - включено
+        0 - выключено
+        """
         super().value(v)
         self.is_on = bool(v)
 
     def on(self):
+        """
+        Включает светодиод.
+        """
         self.value(1)
 
     def off(self):
+        """
+        Выключает светодтод.
+        """
         self.value(0)
 
     def high(self):
+        """
+        Включает светодиод.
+        """
         self.value(1)
 
     def low(self):
+        """
+        Выключает светодтод.
+        """
         self.value(0)
 
     def toggle(self):
+        """
+        Переключает режим работы светодиода с ВЫКЛ на ВКЛ и наоборот.
+        """
         if self.is_on:
             self.value(0)
         else:
